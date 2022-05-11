ab2aad7c6931d317f0188886af1d9f3ff1352cca
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2022-05-11 19:47:15 -0300

    Commit 2

--- src/test/Test.Library/KnightTest.cs
+++ src/test/Test.Library/KnightTest.cs
@@ -15,5 +15,31 @@ namespace Test.Library
             IItem armor = new Armor();
             Assert.AreEqual(TestKnight.Name,"Test");
         }
+
+        [Test]
+
+        public void KnightAttack()
+        {
+            Knight TestKnight = new Knight("Test");
+            IItem sword = new Sword();
+            IItem shield = new Shield();
+            IItem armor = new Armor();
+            int attack = sword.AttackValue;
+            int expected = 20;
+            Assert.AreEqual(expected,attack);
+        }
+
+        [Test]
+
+        public void KnightDefense()
+        {
+            Knight TestKnight = new Knight("Test");
+            IItem sword = new Sword();
+            IItem shield = new Shield();
+            IItem armor = new Armor();
+            int defense = shield.DefenseValue + armor.DefenseValue;
+            int expected = 39;
+            Assert.AreEqual(expected,defense);
+        }
     }
 }
