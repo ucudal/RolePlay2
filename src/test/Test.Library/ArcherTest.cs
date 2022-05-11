eed99913668c2d43b8bb4996850afef1ae7d14e5
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2022-05-11 19:47:15 -0300

    Commit 2

--- src/test/Test.Library/ArcherTest.cs
+++ src/test/Test.Library/ArcherTest.cs
@@ -16,5 +16,30 @@ namespace Test.Library
             IItem helmet = new Helmet();
             Assert.AreEqual(TestArcher.Name,"Test");
         }
+
+        [Test]
+
+        public void ArcherAttack()
+        {
+            Archer TestArcher = new Archer("Test");
+            IItem bow = new Bow();
+            IItem helmet = new Helmet();
+            int attack = bow.AttackValue;
+            int expected = 15;
+            Assert.AreEqual(expected,attack);
+        }
+
+        [Test]
+
+        public void KnightDefense()
+        {
+            Archer TestArcher = new Archer("Test");
+            IItem bow = new Bow();
+            IItem helmet = new Helmet();
+            int defense = helmet.DefenseValue;
+            int expected = 18;
+            Assert.AreEqual(expected,defense);
+        }
+
     }
 }
--- src/test/Test.Library/DwarfTest.cs
+++ src/test/Test.Library/DwarfTest.cs
@@ -14,5 +14,29 @@ namespace Test.Library
             IItem helmet = new Helmet();
             Assert.AreEqual(TestDwarf.Name,"Test");
         }
+
+        [Test]
+
+        public void DwarfAttack()
+        {
+            Dwarf TestDwarf = new Dwarf("Test");
+            IItem axe = new Axe();
+            IItem helmet = new Helmet();
+            int attack = axe.AttackValue;
+            int expected = 25;
+            Assert.AreEqual(expected,attack);
+        }
+
+        [Test]
+
+        public void DwarfDefense()
+        {
+            Dwarf TestDwarf = new Dwarf("Test");
+            IItem axe = new Axe();
+            IItem helmet = new Helmet();
+            int defense = helmet.DefenseValue;
+            int expected = 18;
+            Assert.AreEqual(expected,defense);
+        }
     }
 }
